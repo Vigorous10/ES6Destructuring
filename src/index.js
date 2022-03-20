036c8ada516f4a3bb37ebc0d5fc657bf2cf2b7b9
Harshjeet raj <85995119+Vigorous10@users.noreply.github.com>
2022-03-20 09:36:34 +0530

    inserted destructing file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+import animals, { useAnimals } from "./data";
+
+// Destructuring Arrays
+// console.log(animals);
+
+const [cat, dog] = animals;
+// console.log(cat);
+
+const [animal, makeSound] = useAnimals(cat);
+console.log(animal);
+makeSound();
+
+//const {name,sound}=cat;
+
+//const{name: catName,sound: catSound}=cat;
+
+//const{name="fluffy",sound="Purr"}=cat;
+
+// const{feedingRequirements:{food,water}}=cat;
+
+// console.log(food);
+
 // CHALLENGE: uncomment the code below and see the car stats rendered
 // import React from "react";
 // import ReactDOM from "react-dom";
