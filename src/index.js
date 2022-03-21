249aba338983cc00e31385b6a11ddd3520ed0054
Harshjeet raj <85995119+Vigorous10@users.noreply.github.com>
2022-03-21 08:32:08 +0530

    added js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import animals, { useAnimals } from "./data";
+// import animals, { useAnimals } from "./data";
 
 // Destructuring Arrays
 // console.log(animals);
 
-const [cat, dog] = animals;
+// const [cat, dog] = animals;
 // console.log(cat);
 
-const [animal, makeSound] = useAnimals(cat);
-console.log(animal);
-makeSound();
+// const [animal, makeSound] = useAnimals(cat);
+// console.log(animal);
+// makeSound();
 
 //const {name,sound}=cat;
 
@@ -21,25 +21,43 @@ makeSound();
 // console.log(food);
 
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const [honda, tesla] = cars;
+
+const {
+  speedStats: { topspeed: hondaTopSpeed }
+} = honda;
+const {
+  speedStats: { topspeed: teslaTopSpeed }
+} = tesla;
+
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour]
+} = tesla;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
